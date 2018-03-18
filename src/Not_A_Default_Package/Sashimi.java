6545fa89ccb2c7be9a0b8f969ffae0c99898bb4f
elon61 <elon61@users.noreply.github.com>
2018-03-18 15:07:56 +0200

    I NEED MESSAGE

---
+++ src/Not_A_Default_Package/Sashimi.java
@@ -0,0 +1,23 @@
+package Not_A_Default_Package;
+
+public class Sashimi {
+	snobord board;
+	int x, y, z;
+
+	public Sashimi(int x, int y, int z) { // board size and win size
+		this.x = x; this.y = y; this.z = z;
+		board = new snobord(x, y, z);
+	}
+	
+	public boolean play(int x, int y, Player p) { // played location and player who played there
+		return board.playBrocoli(x, y, p);
+	}
+	
+	public boolean win(int x, int y, Player p) {
+		return board.brocoliest(p, x, y);
+	}
+	
+	public Celullose[][] getBoard() {
+		return board.getBrocoli();
+	}
+}
